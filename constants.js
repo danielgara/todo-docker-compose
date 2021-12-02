75c6a58b732e83c9d5aa7bcba52283e36df93794
Daniel Correa <yo@danielgara.com>
2021-12-02 11:30:15 -0500

    first commit

---
+++ constants.js
@@ -0,0 +1,15 @@
+const DATABASE_NAME = 'todo_docker_1';//'db_todo';
+const USERNAME = 'root';
+const PASSWORD = 'root';
+const HOST = 'mysqldb';
+const DIALECT = 'mysql';
+const PORT = '3306';
+
+module.exports = {
+  DATABASE_NAME: DATABASE_NAME,
+  USERNAME: USERNAME,
+  PASSWORD: PASSWORD,
+  HOST: HOST,
+  DIALECT: DIALECT,
+  PORT: PORT,
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+ const multer = require('multer');
+
+/* include controllers */
+const homeController = require("./controllers/homeController");
+const todoController = require("./controllers/todoController");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+ app.use(multer({ dest: '/public/uploads/' }).any());
+
+/* Routes */
+app.get('/', homeController.home);
+app.get('/about', homeController.about);
+app.get('/todos', todoController.list);
+ app.get('/todosOrdered', todoController.listOrdered);
+app.post('/todos', todoController.add);
+app.get('/todos/delete/:id', todoController.delete);
+app.get('/todos/update/:id/:state', todoController.update);
+
+app.use(express.static("public"));
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
